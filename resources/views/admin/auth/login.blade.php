d8ec00e7f3494b44c1e009afe78ee3cb476dab86
Shesh Kumar Mishra <mishrakshesh14287@gmail.com>
2020-07-09 00:09:05 +0530

    catgories updated again

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -4,9 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/main.css') }}" />
+    <!--<link rel="stylesheet" type="text/css" href="{{ asset('backend/css/main.css') }}" />
     <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/font-awesome/4.7.0/css/font-awesome.min.css') }}"/>
-    <title>Login - {{ config('app.name') }}</title>
+   --> <title>Login - {{ config('app.name') }}</title>
 </head>
 <body>
 <section class="material-half-bg">
